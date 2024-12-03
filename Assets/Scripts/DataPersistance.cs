62e5f3df8bd7bfa13c8d4f6bfaa4993849d4ec16
Almosk <117522876+Alvin-Kita@users.noreply.github.com>
2024-12-03 12:21:44 +0000

    creation dataPersistance (#2)
    Co-authored-by: Remi P <perez.remi@gmail.com>

---
+++ Assets/Scripts/DataPersistance.cs
@@ -0,0 +1,19 @@
+//using UnityEngine;
+
+public class DataPersistance
+{
+    public static string Pseudo = ""; // Variable partagée entre les scènes
+      
+} 
+    /*// Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
