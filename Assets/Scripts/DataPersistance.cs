b738f8029bb3f646d4f5eb83b00065e19265f2da
Remi P <perez.remi@gmail.com>
2024-12-15 11:52:19 +0100

    sauvegarde dans fichier Json

--- Assets/Scripts/DataPersistance.cs
+++ Assets/Scripts/DataPersistance.cs
@@ -3,6 +3,7 @@
 public class DataPersistance
 {
     public static string Pseudo = ""; // Variable partagée entre les scènes
+    public static int Score = 0;
     public static int numberScenes = 1;
       
 } 
