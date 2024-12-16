5cf2e61416402d68f98e0d0c4e3b20fbaea01abd
Tokkyoo <zydrox6910@gmail.com>
2024-12-16 18:43:56 +0100

    gestion des enemy & score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 1; // Points de vie de l'ennemi
+
+    public int points = 10; // Points ajoutés au score quand détruit
+
+    private ScoreManager scoreManager;
+
+
+    private void Start()
+    {
+        // Trouve dynamiquement le ScoreManager dans la scène
+        scoreManager = FindObjectOfType<ScoreManager>();
+
+        if (scoreManager == null)
+        {
+            Debug.LogError("ScoreManager introuvable dans la scène !");
+        }
+    }
+
 
     public void TakeDamage(int damage)
     {
@@ -12,9 +30,15 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
-
     void Die()
     {
+
+        if (scoreManager != null)
+        {
+            Debug.Log("Enemy Killed! ajout de" + points);
+            scoreManager.AddScore(points);
+        }
+
         Debug.Log("Enemy Killed!");
         Destroy(gameObject); // Détruit l'ennemi
     }
