c20199eb4a739d01c207351a7cb2fae7188dc5b9
alvin-kita <alvin.kita@outlook.fr>
2024-12-16 11:20:49 +0100

    Fix: DataPersistance

--- Assets/Scripts/DataPersistance.cs
+++ Assets/Scripts/DataPersistance.cs
@@ -3,17 +3,10 @@
 public class DataPersistance
 {
     public static string Pseudo = ""; // Variable partagée entre les scènes
-      
-} 
-    /*// Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
+
+    // partagée entre les scènes
+    public static int Score = 0;
+    public static int numberScenes = 1;
+} 
 
